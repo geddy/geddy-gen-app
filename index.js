60be61cd5fe60d5d8c1c644c79b29d5ec657b534
Ondrej Brinkel <info@anzui.de>
2014-01-24 00:16:50 +0100

    typo fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(appPath, args) {
 
   var validTasks = ['create','migrate','update','upgradePrevious'];
 
-  // keep support of old style gen syntax with
+  // keep support of old style gen syntax
   if (args.length > 0 && validTasks.indexOf(args[0]) === -1) {
     console.warn('This syntax is deprecated and will not be supported in future versions.\nThe new syntax is: geddy gen app create[app-name].\n');
     args = ['create[' + args.join(',') + ']'];
