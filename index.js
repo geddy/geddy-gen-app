ed6a8d52fec9d2eb56971e1f53787e111bbb0490
Ondrej Brinkel <info@anzui.de>
2014-02-12 10:44:58 +0100

    minor bugfix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-module.exports = function(appPath, args) {
-  var jake = require('jake');
-  var path = require('path');
+var jake = require('jake');
+var path = require('path');
 
+module.exports = function(appPath, args) {
   var validTasks = ['create','migrate','update','upgradePrevious'];
 
   // keep support of old style gen syntax
-  if (args.length > 0 && validTasks.indexOf(args[0]) !== -1) {
+  if (args.length > 0 && validTasks.indexOf(args[0]) === -1) {
     console.warn('This syntax is deprecated and will not be supported in future versions.\nThe new syntax is: geddy gen app create[app-name].\n');
     args = ['default[' + args.join(',') + ']'];
   }
