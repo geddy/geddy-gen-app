6813bb52812c3902df28f5114c22e662de4341ba
Ondrej Brinkel <info@anzui.de>
2014-07-29 21:19:31 +0200

    supporting programatic calling of tasks

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var jake = require('jake');
 var path = require('path');
+var validTasks = ['create','migrate','update','upgradePrevious'];
 
 module.exports = function(appPath, args) {
-  var validTasks = ['create','migrate','update','upgradePrevious'];
-
   // keep support of old style gen syntax
   if (args.length > 0 && validTasks.indexOf(args[0]) === -1) {
     console.warn('This syntax is deprecated and will not be supported in future versions.\nThe new syntax is: geddy gen app create[app-name].\n');
@@ -22,3 +21,10 @@ module.exports = function(appPath, args) {
   // run our tasks
   jake.run.apply(jake, args);
 }
+
+validTasks.forEach(function(task) {
+  module.exports[task] = function() {
+    var t = jake.Task.create;
+    t.invoke.apply(t, Array.prototype.slice.call(arguments));
+  }
+});
